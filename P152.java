5c00f844e13a9b6a43e24e6e90eb5d64a5ac8104
Riddhi Kabra <72146252+RiddhiKabra@users.noreply.github.com>
2021-10-15 20:41:15 +0530

    Add files via upload

---
+++ P152.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class P152
+{
+    public static void main(String[] args)
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue=0;
+        boolean bRet=false;
+        System.out.println("Enter  number:");
+        iValue=sobj.nextInt();
+        Marvellous mobj= new Marvellous();
+         bRet= mobj.CheckPrime(iValue);
+         if(bRet==true)
+         {
+            System.out.println("Number is Prime");
+         }
+         else
+         {
+            System.out.println("Number is not Prime");
+         } 
+    }
+}
+
+class Marvellous
+{
+    public boolean CheckPrime(int iNo)
+    {
+      int iCnt=0;
+      boolean bFlag=true;
+      for(iCnt=2;iCnt<=iNo/2;iCnt++)
+      {
+        if((iNo%iCnt)==0)
+        {
+            bFlag= false;
+            break;
+        }
+      }
+      return bFlag;
+    }   
+}
