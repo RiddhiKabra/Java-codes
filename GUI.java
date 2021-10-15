b24ba992205a1c3a6320fd74420ac9495dc87583
Riddhi Kabra <72146252+RiddhiKabra@users.noreply.github.com>
2021-10-15 20:36:54 +0530

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,10 @@
+import  java.awt.*;
+class GUI
+{
+   public static void main(String[] args) 
+   {
+  	Frame fobj=new Frame("Marvelllous");
+  	fobj.setSize(500,500);
+  	fobj.setVisible(true);
+  }
+}
